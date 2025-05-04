be50299078e6111320c4d095e3907f33e73183d6
Peter Carey <petermarkcarey@gmail.com>
2025-05-04 11:35:36 +0800

    connect nodes test

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,26 @@ import { nodeTypes } from './nodes'
 const { onConnect, addEdges } = useVueFlow()
 
 const nodes = ref<Node[]>([
-  { id: '3', type: 'custom', position: { x: 400, y: 100 } },
-  { id: '4', type: 'debug-log', position: { x: 300, y: 300 } },
+  { id: '2', type: 'run', position: { x: 100, y: 100 } },
+  { id: '3', type: 'custom', position: { x: 100, y: 300 } },
+  { id: '4', type: 'debug-log', position: { x: 300, y: 100 } },
 ])
 
 const edges = ref<Edge[]>([
+  { 
+    id: 'e1->2',
+    source: '2', 
+    target: '4',
+    sourceHandle: 'source-Exec',
+    targetHandle: 'target-Exec'
+  },
 ])
 
 onConnect((params) => {
   addEdges([params])
 })
+
+
 </script>
 
 <template>
--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -1,8 +1,10 @@
 import DebugLogNode from './DebugLogNode.vue'
 import CustomNode from './CustomNode.vue'
+import EntryNode from './EntryNode.vue'
 
 export const nodeTypes = [
     { type: 'debug-log', component: DebugLogNode },
-    { type: 'custom', component: CustomNode }
+    { type: 'custom', component: CustomNode },
+    { type: 'run', component: EntryNode }
 ]
 
