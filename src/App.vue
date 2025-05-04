212e9b3de24b89a74d81b8287d8e0b3df30185a7
Peter Carey <petermarkcarey@gmail.com>
2025-05-04 11:27:31 +0800

    node registration through index.ts file

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,13 @@ import { Background, BackgroundVariant } from '@vue-flow/background'
 import { Controls } from '@vue-flow/controls'
 import { MiniMap } from '@vue-flow/minimap'
 import { VueFlow, useVueFlow, type Node, type Edge, ConnectionMode } from '@vue-flow/core'
-import CustomNode from './CustomNode.vue'
 import CustomEdge from './CustomEdge.vue'
-import DebugLogNode from './nodes/DebugLogNode.vue'
+import { nodeTypes } from './nodes'
 
 const { onConnect, addEdges } = useVueFlow()
 
 const nodes = ref<Node[]>([
-  { id: '3', type: 'custom', label: 'Node 3', position: { x: 400, y: 100 } },
+  { id: '3', type: 'custom', position: { x: 400, y: 100 } },
   { id: '4', type: 'debug-log', position: { x: 300, y: 300 } },
 ])
 
@@ -41,14 +40,11 @@ onConnect((params) => {
 
       <Controls />
 
-      <template #node-custom="nodeProps">
-        <CustomNode v-bind="nodeProps" />
-      </template>
-      <template #node-debug-log="nodeProps">
-        <DebugLogNode v-bind="nodeProps" />
-      </template>
-      <template #edge-custom="edgeProps">
-        <CustomEdge v-bind="edgeProps" />
+      <template
+        v-for="node in nodeTypes"
+        :key="node.type"
+        #[`node-${node.type}`]="nodeProps">
+        <component :is="node.component" v-bind="nodeProps" />
       </template>
     </VueFlow>
   </div>
---
+++ src/nodes/index.ts
@@ -0,0 +1,8 @@
+import DebugLogNode from './DebugLogNode.vue'
+import CustomNode from './CustomNode.vue'
+
+export const nodeTypes = [
+    { type: 'debug-log', component: DebugLogNode },
+    { type: 'custom', component: CustomNode }
+]
+
